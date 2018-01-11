7f9fa750a94963df29df1241f5ed28c8d9109130
hermawanadin <aandzero@gmail.com>
2018-01-11 17:48:27 +0700

    Add files via upload

---
+++ timeline_twitter.r
@@ -0,0 +1,56 @@
+﻿install.packages(“twitteR”)
+install.packages(“ROAuth”)
+install.packages(“tm”)
+install.packages(“ggplot2”)
+install.packages(“wordcloud”)
+
+library(twitteR)
+library(ROAuth)
+library(tm)
+library(ggplot2)
+library(wordcloud)
+setup_twitter_oauth(“consumer_key”, “cosumer_secret_key”, “access_token_key”,”access_token_secret_key”)
+
+tweets <- userTimeline(“Banjir”, n = 50)
+n.tweet <- length(tweets)
+# convert tweets to a data frame
+tweets.df <- twListToDF(tweets)
+
+myCorpus <- Corpus(VectorSource(tweets.df$text))
+# convert to lower case
+myCorpus <- tm_map(myCorpus, content_transformer(tolower))
+# remove URLs
+removeURL <- function(x) gsub(“http[^[:space:]]*”, “”, x)
+myCorpus <- tm_map(myCorpus, content_transformer(removeURL))
+# remove anything other than English letters or space
+removeNumPunct <- function(x) gsub(“[^[:alpha:][:space:]]*”, “”, x)
+myCorpus <- tm_map(myCorpus, content_transformer(removeNumPunct))
+# remove stopwords
+myStopwords <- c(setdiff(stopwords(‘english’), c(“r”, “big”)),”use”, “see”, “used”, “via”, “amp”)
+myCorpus <- tm_map(myCorpus, removeWords, myStopwords)
+# remove extra whitespace
+myCorpus <- tm_map(myCorpus, stripWhitespace)
+# keep a copy for stem completion later
+myCorpusCopy <- myCorpus
+
+tdm <- TermDocumentMatrix(myCorpus)
+
+term.freq <- rowSums(as.matrix(tdm))
+
+term.freq <- subset(term.freq, term.freq >= 20)
+df <- data.frame(term = names(term.freq), freq = term.freq)
+
+ggplot(df, aes(x=term, y=freq)) + geom_bar(stat=”identity”) +
+xlab(“Terms”) + ylab(“Count”) + coord_flip() +
+theme(axis.text=element_text(size=7))
+
+m <- as.matrix(tdm)
+# calculate the frequency of words and sort it by frequency
+word.freq <- sort(rowSums(m), decreasing = T)
+# colors
+pal <- brewer.pal(9, “BuGn”)[-(1:4)]
+
+# plot word cloud
+
+wordcloud(words = names(word.freq), freq = word.freq, min.freq = 3,
+random.order = F, colors = pal)
